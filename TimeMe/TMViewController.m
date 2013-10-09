8d03749de35f6c543c84a4b43a87bab66208aca0
Kenneth Barry <kennetcb@usc.edu>
2013-10-09 13:18:54 -0700

    Removed references to colors, and toggle section of tableview

--- TimeMe/TMViewController.m
+++ TimeMe/TMViewController.m
@@ -97,33 +97,29 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath { // should be lightweight
     UITableViewCell *cell = nil;
-    if (indexPath.section != 2) { //we're a picker section
-        NSTimeInterval timeInterval = (indexPath.section == TIMER_VIEW_TAG) ? _timer.timerLength : _timer.intervalLength;
-        if (indexPath.row == 0) { //we display info on the timer, not the timer picker itself
-            static NSString *kTimerPickerTitleCellID = @"timercelltitlepickerid";
-            cell = [tableView dequeueReusableCellWithIdentifier:kTimerPickerTitleCellID];
-            if (!cell) {
-                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
-                                              reuseIdentifier:kTimerPickerTitleCellID];
-            }
-            NSString *titleText = (indexPath.section == TIMER_VIEW_TAG) ? @"Timer Length" : @"Timer Interval";
-            [cell.textLabel setText:titleText];
-            
-            NSString *intervalString = [self _stringForCountdownTime:timeInterval];
-            [cell.detailTextLabel setText:intervalString];
-            cell.contentView.backgroundColor = [UIColor colorWithRed:0.5f green:0.0f blue:0.5f alpha:0.15f];
-            cell.textLabel.textColor = [UIColor purpleColor];
-        } else { //display a pickerview for this one
-            static NSString *kPickerViewCellID = @"pickerviewcellid";
-            cell = [tableView dequeueReusableCellWithIdentifier:kPickerViewCellID];
-            if (!cell) {
-                cell = [[TMTimePickerCell alloc] initWithReuseIdentifier:kPickerViewCellID];
-                ((TMTimePickerCell *)cell).delegate = self;
-                [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
-            }
-            [((TMTimePickerCell *)cell) configureForTimeInterval:timeInterval];
-            cell.tag = indexPath.section;
+    NSTimeInterval timeInterval = (indexPath.section == TIMER_VIEW_TAG) ? _timer.timerLength : _timer.intervalLength;
+    if (indexPath.row == 0) { //we display info on the timer, not the timer picker itself
+        static NSString *kTimerPickerTitleCellID = @"timercelltitlepickerid";
+        cell = [tableView dequeueReusableCellWithIdentifier:kTimerPickerTitleCellID];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1
+                                          reuseIdentifier:kTimerPickerTitleCellID];
         }
+        NSString *titleText = (indexPath.section == TIMER_VIEW_TAG) ? @"Timer Length" : @"Timer Interval";
+        [cell.textLabel setText:titleText];
+        
+        NSString *intervalString = [self _stringForCountdownTime:timeInterval];
+        [cell.detailTextLabel setText:intervalString];
+    } else { //display a pickerview for this one
+        static NSString *kPickerViewCellID = @"pickerviewcellid";
+        cell = [tableView dequeueReusableCellWithIdentifier:kPickerViewCellID];
+        if (!cell) {
+            cell = [[TMTimePickerCell alloc] initWithReuseIdentifier:kPickerViewCellID];
+            ((TMTimePickerCell *)cell).delegate = self;
+            [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+        }
+        [((TMTimePickerCell *)cell) configureForTimeInterval:timeInterval];
+        cell.tag = indexPath.section;
     }
     return cell;
 }
@@ -169,9 +165,6 @@
 
 - (void)intervalTimerDidFinishTimer:(TMIntervalTimer *)intervalTimer {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UITableViewCell *toggleCell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:2]];
-        toggleCell.textLabel.textColor = [UIColor greenColor];
-        [toggleCell.textLabel setText:@"Start Timer"];
     });
 }
 
@@ -181,7 +174,6 @@
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:timePickerCell.tag]];
     NSString *intervalString = [self _stringForCountdownTime:timeInterval];
     [cell.detailTextLabel setText:intervalString];
-    cell.tintColor = [UIColor purpleColor];
     
     if (timePickerCell.tag == INTERVAL_VIEW_TAG) {
         [_timer setIntervalLength:timeInterval];
