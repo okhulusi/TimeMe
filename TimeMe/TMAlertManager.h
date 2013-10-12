0485540ec4369efddb8423c340193bd3d708f2cc
Kenneth Barry <kennetcb@usc.edu>
2013-10-12 10:35:32 -0700

    Added alertmanager

---
+++ TimeMe/TMAlertManager.h
@@ -0,0 +1,15 @@
+//
+//  TMAlertManager.h
+//  TimeMe
+//
+//  Created by Clark Barry on 10/12/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMAlertManager : NSObject
+
+- (instancetype)getInstance;
+
+@end
---
+++ TimeMe/TMAlertManager.m
@@ -0,0 +1,24 @@
+//
+//  TMAlertManager.m
+//  TimeMe
+//
+//  Created by Clark Barry on 10/12/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import "TMAlertManager.h"
+
+@implementation TMAlertManager
+
+static TMAlertManager *__instance = nil;
+- (instancetype)getInstance {
+    if (!__instance) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            __instance = [[TMAlertManager alloc] init];
+        });
+    }
+    return __instance;
+}
+
+@end
