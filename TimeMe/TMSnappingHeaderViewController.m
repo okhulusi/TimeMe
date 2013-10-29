8212457a3397991349d781df01029b0d5f0088d7
Kenneth Barry <kennetcb@usc.edu>
2013-10-29 15:15:08 -0700

    Added snapping header class

---
+++ TimeMe/TMSnappingHeaderViewController.m
@@ -0,0 +1,38 @@
+//
+//  TMSnappingHeaderTableViewViewController.m
+//  Bzz
+//
+//  Created by Clark Barry on 10/29/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import "TMSnappingHeaderViewController.h"
+
+@interface TMSnappingHeaderViewController ()
+
+@end
+
+@implementation TMSnappingHeaderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
