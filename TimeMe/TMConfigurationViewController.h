2cf6c60e99c3739e6681ee4be5ec2551b197261e
Kenneth Barry <kennetcb@usc.edu>
2013-11-29 19:10:19 -0800

    Added configurationviewcontroller

---
+++ TimeMe/TMConfigurationViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMConfigurationViewController.h
+//  Bzz
+//
+//  Created by Clark Barry on 11/29/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMConfigurationViewController : UITableViewController
+
+@end
