cfd68ad05c3a57395548c96b7bd6ec2280630b9b
Kenneth Barry <kennetcb@usc.edu>
2013-11-29 19:22:00 -0800

    Added configuration cell

---
+++ TimeMe/TMConfigurationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TMConfigurationTableViewCell.h
+//  Bzz
+//
+//  Created by Clark Barry on 11/29/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import "TMTableViewCell.h"
+
+@interface TMConfigurationTableViewCell : TMTableViewCell
+
+@end
---
+++ TimeMe/TMConfigurationTableViewCell.m
@@ -0,0 +1,31 @@
+//
+//  TMConfigurationTableViewCell.m
+//  Bzz
+//
+//  Created by Clark Barry on 11/29/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import "TMConfigurationTableViewCell.h"
+
+@implementation TMConfigurationTableViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
