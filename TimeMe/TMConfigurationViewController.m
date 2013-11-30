faeaa7262318ab086ee8a403ee28ed00294ed19f
Kenneth Barry <kennetcb@usc.edu>
2013-11-29 20:17:48 -0800

    hooked list view controller up with add and done button

--- TimeMe/TMConfigurationViewController.m
+++ TimeMe/TMConfigurationViewController.m
@@ -9,19 +9,42 @@
 #import "TMConfigurationViewController.h"
 #import "TMConfigurationTableViewCell.h"
 #import "TMConfigurationManager.h"
+#import "TMStyleManager.h"
+#import "TMTimerConfiguration.h"
 
 @interface TMConfigurationViewController ()
-
+- (void)_doneButtonPressed;
+- (void)_addButtonPressed;
 @end
 
 @implementation TMConfigurationViewController
 
-- (id)initWithStyle:(UITableViewStyle)style {
-    self = [super initWithStyle:style];
-    if (self) {
-        [self setTitle:@"Bzz"];
-    }
-    return self;
+- (void)_addButtonPressed {
+    TMTimerConfiguration *configuration = [[TMTimerConfiguration alloc] init];
+    TMConfigurationManager *configurationManager = [TMConfigurationManager getInstance];
+    [configurationManager addTimerConfiguration:configuration];
+    NSInteger rowCount = [self.tableView numberOfRowsInSection:0];
+    NSIndexPath *insertPath = [NSIndexPath indexPathForRow:rowCount inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[insertPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+    
+}
+
+- (void)_doneButtonPressed {
+    [self.navigationController.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)loadView {
+    [super loadView];
+    UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(_doneButtonPressed)];
+    [self.navigationItem setLeftBarButtonItem:doneButton];
+    
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(_addButtonPressed)];
+    [self.navigationItem setRightBarButtonItem:addButton];
+    TMStyleManager *styleManager = [TMStyleManager getInstance];
+    [self.view setBackgroundColor:styleManager.backgroundColor];
+    [self.tableView setBackgroundColor:styleManager.backgroundColor];
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    
 }
 
 #pragma mark - Table view data source
@@ -30,16 +53,22 @@
     return 1;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     TMConfigurationManager *configurationManager = [TMConfigurationManager getInstance];
     return [configurationManager.configurations count];
-
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *kConfigurationCellId = @"Configurationcellid";
-    TMConfigurationTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kConfigurationCellId forIndexPath:indexPath];
+    TMConfigurationTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kConfigurationCellId];
+    if (!cell) {
+        cell = [[TMConfigurationTableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:kConfigurationCellId];
+    }
     TMConfigurationManager *configurationManager = [TMConfigurationManager getInstance];
     TMTimerConfiguration *configuration = [configurationManager.configurations objectAtIndex:indexPath.row];
     [cell configureForTimerConfiguration:configuration];
