34c30da432ba4637ae529a474262eed1eb3dd011
Kenneth Barry <kennetcb@usc.edu>
2013-12-01 09:03:34 -0800

    Added section header

---
+++ TimeMe/TMSectionHeader.h
@@ -0,0 +1,13 @@
+//
+//  TMSectionHeader.h
+//  Bzz
+//
+//  Created by Clark Barry on 12/1/13.
+//  Copyright (c) 2013 KCBODK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMSectionHeader : UIView
+
+@end
